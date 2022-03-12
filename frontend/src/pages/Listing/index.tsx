5ee435b2c06f83549a45ce1d2213776416b1645f
Paulo Vinicius Silva Costa <pauloviniciius@outlook.com>
2022-03-12 20:13:50 -0300

    import axios, implement method get test

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,22 @@
+
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { BASE_URL } from "utils/requests";
+
 
 function Listing() {
+
+  //forma errada
+    axios.get(`${BASE_URL}/movie?size=12&page=0`).then(
+      element =>{
+        console.log(element)
+      });
+
   return (
+
+    
+
     <>
       <Pagination />
       <div className="container">
